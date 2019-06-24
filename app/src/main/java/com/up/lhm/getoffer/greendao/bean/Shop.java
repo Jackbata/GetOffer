18c60a06ccff01839de615b921fdd25dad1de398
lianghaimiao <lianghaimiao@qfpay.com>
2019-06-24 17:39:25 +0800

    增加greendao案例

---
+++ app/src/main/java/com/up/lhm/getoffer/greendao/bean/Shop.java
@@ -0,0 +1,10 @@
+package com.up.lhm.getoffer.greendao.bean;
+
+/**
+ * @author Barry
+ * @date 2019-06-24
+ * @function
+ */
+
+public class Shop {
+}
---
+++ app/src/main/java/com/up/lhm/getoffer/greendao/dao/ShopDao.java
@@ -0,0 +1,10 @@
+package com.up.lhm.getoffer.greendao.dao;
+
+/**
+ * @author Barry
+ * @date 2019-06-24
+ * @function
+ */
+
+public class ShopDao {
+}
